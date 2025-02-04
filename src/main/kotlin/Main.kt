3ac75a9ba55a32be6ffd0dcfcf1de41e8b020506
Bulat G <dzabroadd@gmail.com>
2025-02-04 20:45:36 +0500

    KS-1-2-var-print

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+package org.example
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    val name = "Kotlin"
+
+    println("Hello, " + name + "!")
+
+    for (i in 1..9) {
+        println("I like $name")
+    }
+}
---
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -0,0 +1,17 @@
+package org.example.lesson_1
+
+fun main(){
+    val offerCount : Int = 75
+    val gratitudeText: String = "Спасибо за покупку."
+
+    println(offerCount)
+    println(gratitudeText)
+
+    //val numOfEmployees:Int = 2000
+    //println(numOfEmployees)
+
+    val numOfEmployees:Int = 1999
+    println(numOfEmployees)
+
+
+}
